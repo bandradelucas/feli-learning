f9463b6aabb028dfc0aa0f5649602e7bd0a03a7c
Lucas de Andrade <bandradelucas@gmail.com>
2022-06-10 16:11:14 -0300

    dev

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,10 @@ const port = process.env.PORT;
 app.get('/', (req: Request, res: Response) => {
   // res.send(process.env.MONGO_CONNECTION);
   mongoose.connect(process.env.DB_CONNECTION);
+  const Cat = mongoose.model('Cat', { name: String });
+
+  const kitty = new Cat({ name: 'Zildjian' });
+  kitty.save().then(() => console.log('meow'));
 
   res.send('Express + TypeScript Server');
 });
